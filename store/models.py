d2cb869f3d02953d5a17ec1545b183726f2d51f5
TikaThamsuhang <tikathamsuhang1@gmail.com>
2025-12-05 14:42:14 +0545

    impleted get_url for Product and setup proper links

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from category.models import Category
+from django.urls import reverse
 
 # Create your models here.
 class Product(models.Model):
@@ -15,6 +16,23 @@ class Product(models.Model):
 
     created_date = models.DateTimeField(auto_now_add=True) # auto_now_add=True means it will add the current time when the object is created
     modified_date = models.DateTimeField(auto_now=True) # auto_now=True means it will update the current time when the object is modified
+
+    def get_url(self):
+        # reverse() generates a URL by looking at the name given in urls.py.
+        # Here, 'product_detail' is the name of the URL pattern used
+        # for displaying a single product's detail page.
+        
+        # 'args' provides the dynamic parts of the URL.
+        # In your urls.py, your product detail URL likely looks like this:
+        # path('category/<slug:category_slug>/<slug:product_slug>/', ...)
+        #
+        # So we need to provide 2 values in order:
+        # 1. category_slug  → self.category.slug (slug of the product's category)
+        # 2. product_slug   → self.slug (slug of the product itself)
+        
+        # This returns a complete URL like:
+        # /category/electronics/iphone-14-pro/
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
